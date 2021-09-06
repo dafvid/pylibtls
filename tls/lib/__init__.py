f718f56f5324f4b8a49a0decbda4f3eef5db2083
David Stenwall <david@dafnet.se>
2021-09-07 00:34:14 +0200

    Add more config variables

--- tls/lib/__init__.py
+++ tls/lib/__init__.py
@@ -1,11 +1,8 @@
 import sys
 
-from ctypes import CDLL, c_int, c_char_p, c_void_p, c_size_t, c_ssize_t
+from ctypes import CDLL, c_int, c_char_p, c_void_p, c_size_t, c_ssize_t, c_uint32
 
 from . import types
-from . import constants
-from . import errors
-
 
 _lib = CDLL('/usr/local/lib/libtls.so')
 
@@ -27,17 +24,60 @@ tls_config_new.restype = types.tls_config_p
 tls_config_free = _lib.tls_config_free
 tls_config_free.argtypes = [types.tls_config_p]
 
+tls_default_ca_cert_file = _lib.tls_default_ca_cert_file
+tls_default_ca_cert_file.restype = c_char_p
+
 tls_config_set_ca_file = _lib.tls_config_set_ca_file
 tls_config_set_ca_file.argtypes = [types.tls_config_p, c_char_p]
 tls_config_set_ca_file.restype = c_int
+
+tls_config_set_ca_path = _lib.tls_config_set_ca_path
+tls_config_set_ca_path.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_ca_path.restype = c_int
 
 tls_config_set_cert_file = _lib.tls_config_set_cert_file
 tls_config_set_cert_file.argtypes = [types.tls_config_p, c_char_p]
 tls_config_set_cert_file.restype = c_int
 
+tls_config_set_ciphers = _lib.tls_config_set_ciphers
+tls_config_set_ciphers.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_ciphers.restype = c_int
+
+tls_config_set_crl_file = _lib.tls_config_set_crl_file
+tls_config_set_crl_file.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_crl_file.restype = c_int
+
+tls_config_set_dheparams = _lib.tls_config_set_dheparams
+tls_config_set_dheparams.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_dheparams.restype = c_int
+
+tls_config_set_ecdhecurve = _lib.tls_config_set_ecdhecurve
+tls_config_set_ecdhecurve.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_ecdhecurve.restype = c_int
+
+tls_config_set_ecdhecurves = _lib.tls_config_set_ecdhecurves
+tls_config_set_ecdhecurves.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_ecdhecurves.restype = c_int
+
 tls_config_set_key_file = _lib.tls_config_set_key_file
 tls_config_set_key_file.argtypes = [types.tls_config_p, c_char_p]
 tls_config_set_key_file.restype = c_int
+
+tls_config_set_ocsp_staple_file = _lib.tls_config_set_ocsp_staple_file
+tls_config_set_ocsp_staple_file.argtypes = [types.tls_config_p, c_char_p]
+tls_config_set_ocsp_staple_file.restype = c_int
+
+tls_config_set_protocols = _lib.tls_config_set_protocols
+tls_config_set_protocols.argtypes = [types.tls_config_p, c_uint32]
+tls_config_set_protocols.restype = c_int
+
+tls_config_set_session_fd = _lib.tls_config_set_session_fd
+tls_config_set_session_fd.argtypes = [types.tls_config_p, c_int]
+tls_config_set_session_fd.restype = c_int
+
+tls_config_set_verify_depth = _lib.tls_config_set_verify_depth
+tls_config_set_verify_depth.argtypes = [types.tls_config_p, c_int]
+tls_config_set_verify_depth.restype = c_int
 
 tls_server = _lib.tls_server
 tls_server.restype = types.tls_p
@@ -60,7 +100,7 @@ tls_accept_socket.argtypes = [types.tls_p, types.tls_pp, c_int]
 tls_accept_socket.restype = c_int
 
 tls_connect_socket = _lib.tls_connect_socket
-tls_connect_socket.argtypes = [types.tls_p, types.tls_pp, c_int]
+tls_connect_socket.argtypes = [types.tls_p, c_int, c_char_p]
 tls_connect_socket.restype = c_int
 
 tls_handshake = _lib.tls_handshake
